a44470c16f443476e336ca132a0af5a97d9bbfef
manouch <manoujon@gmail.com>
2025-11-22 08:24:15 -0600

    End of section 13

--- client/src/features/members/member-list/member-list.ts
+++ client/src/features/members/member-list/member-list.ts
@@ -1,21 +1,99 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal, ViewChild } from '@angular/core';
 import { MemberService } from '../../../core/services/member-service';
-import { Observable } from 'rxjs';
-import { Member } from '../../../types/member';
-import { AsyncPipe } from '@angular/common';
-import { MemberCard } from "../member-card/member-card";
+import { Member, MemberParams } from '../../../types/member';
+import { MemberCard } from '../member-card/member-card';
+import { PaginatedResult } from '../../../types/pagination';
+import { Paginator } from '../../../shared/paginator/paginator';
+import { FilterModal } from '../filter-modal/filter-modal';
 
 @Component({
   selector: 'app-member-list',
-  imports: [AsyncPipe, MemberCard],
+  imports: [MemberCard, Paginator, FilterModal],
   templateUrl: './member-list.html',
-  styleUrl: './member-list.css'
+  styleUrl: './member-list.css',
 })
-export class MemberList {
+export class MemberList implements OnInit {
+  @ViewChild('filterModal') modal!: FilterModal;
   private memberService = inject(MemberService);
-  protected members$: Observable<Member[]>;
+  protected paginatedMembers = signal<PaginatedResult<Member> | null>(null);
+  protected memberParams = new MemberParams();
+  private undatedParams = new MemberParams();
 
   constructor() {
-    this.members$ = this.memberService.getMembers();
+    const filters = localStorage.getItem('filters');
+    if (filters) {
+      this.memberParams = JSON.parse(filters);
+      this.undatedParams = JSON.parse(filters);
+    }
+  }
+
+  ngOnInit(): void {
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this.memberService.getMembers(this.memberParams).subscribe({
+      next: (result) => {
+        this.paginatedMembers.set(result);
+      },
+    });
+  }
+
+  onPageChange(event: { pageNumber: number; pageSize: number }) {
+    this.memberParams.pageSize = event.pageSize;
+    this.memberParams.pageNumber = event.pageNumber;
+    this.loadMembers();
+  }
+
+  openModal() {
+    this.modal.open();
+  }
+
+  onClose() {
+    console.log('Modal closed');
+  }
+
+  onFilterChange(data: MemberParams) {
+    // console.log('Modal submitted data: ', data);
+    this.memberParams = {...data};
+    this.undatedParams = {...data};
+    this.loadMembers();
+  }
+
+  resetFilters() {
+    this.memberParams = new MemberParams();
+    this.undatedParams = new MemberParams();
+    this.loadMembers();
+  }
+
+  get displayMessage(): string {
+    const defaultParams = new MemberParams();
+
+    const filters: string[] = [];
+
+    if (this.undatedParams.gender) {
+      filters.push(this.undatedParams.gender + 's');
+    } else {
+      filters.push('Males, Females');
+    }
+
+    if (
+      this.undatedParams.minAge !== defaultParams.minAge ||
+      this.undatedParams.maxAge !== defaultParams.maxAge
+    ) {
+      filters.push(
+        ` ages ${this.undatedParams.minAge} - ${this.undatedParams.maxAge}`
+      );
+    }
+
+    filters.push(
+      this.undatedParams.orderBy === 'lastActive'
+        ? 'Recently active'
+        : 'Newest members'
+    );
+
+    return filters.length > 0
+      ? `Selected: ${filters.join(' | ')}`
+      : 'All members';
   }
 }
