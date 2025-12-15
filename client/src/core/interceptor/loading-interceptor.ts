62b816218ae4ff8aeaa56a3aa728d61a44c9ae05
manouch <manoujon@gmail.com>
2025-12-15 10:19:20 -0600

    End of section 15

--- client/src/core/interceptor/loading-interceptor.ts
+++ client/src/core/interceptor/loading-interceptor.ts
@@ -9,9 +9,12 @@ export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
   const busyService = inject(BusyService);
 
   const genreateCacheKey = (url: string, params: HttpParams): string => {
-    const paramString = params.keys().map(key => `${key}=${params.get(key)}`).join('&');
+    const paramString = params
+      .keys()
+      .map((key) => `${key}=${params.get(key)}`)
+      .join('&');
     return paramString ? `${url}?${paramString}` : url;
-  }
+  };
 
   const invalidateCache = (urlPattern: string) => {
     for (const key of cache.keys()) {
@@ -20,17 +23,21 @@ export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
         console.log(`Cache invalidated for: ${key}`);
       }
     }
-  }
+  };
 
   const cacheKey = genreateCacheKey(req.url, req.params);
 
   if (req.method.includes('POST') && req.url.includes('/likes')) {
-    invalidateCache('/likes')
+    invalidateCache('/likes');
+  }
+
+  if (req.method.includes('POST') && req.url.includes('/messages')){
+    invalidateCache('/messages');
   }
 
   if (req.method === 'GET') {
     const cachedResponse = cache.get(cacheKey);
-    if(cachedResponse) {
+    if (cachedResponse) {
       return of(cachedResponse);
     }
   }
@@ -39,11 +46,11 @@ export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
 
   return next(req).pipe(
     delay(500),
-    tap(response => {
-      cache.set(cacheKey, response)
+    tap((response) => {
+      cache.set(cacheKey, response);
     }),
     finalize(() => {
-      busyService.idle()
+      busyService.idle();
     })
-  )
+  );
 };
